4c0df007b6fc78f6580909168508651b8e1afbda
dreamskr <43511466+dreamskr@users.noreply.github.com>
2021-05-09 13:37:38 +0000

    Update server.go
    增加模糊查询 目标 (IP:端口)

--- server/server.go
+++ server/server.go
@@ -248,7 +248,7 @@ func GetTunnel(start, length int, typeVal string, clientId int, search string) (
 			if (typeVal != "" && v.Mode != typeVal || (clientId != 0 && v.Client.Id != clientId)) || (typeVal == "" && clientId != v.Client.Id) {
 				continue
 			}
-			if search != "" && !(v.Id == common.GetIntNoErrByStr(search) || v.Port == common.GetIntNoErrByStr(search) || strings.Contains(v.Password, search) || strings.Contains(v.Remark, search)) {
+			if search != "" && !(v.Id == common.GetIntNoErrByStr(search) || v.Port == common.GetIntNoErrByStr(search) || strings.Contains(v.Password, search) || strings.Contains(v.Remark, search) || strings.Contains(v.Target.TargetStr, search)) {
 				continue
 			}
 			cnt++
